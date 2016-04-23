b1e98e2531053926d47d7a948b3a1e3bdb1c86b6
Tomás Boccardo <tomasboccardo@gmail.com>
2016-04-23 14:04:28 -0300

    Adding some tests on PtdTodoList. Notify mechanism for priority changes on PtdTask

--- ptd/model/todo.py
+++ ptd/model/todo.py
@@ -1,24 +1,58 @@
+from heapq import heappush, heapify
+
+from ptd.util.exceptions import DuplicatedIdException
+
+
 class PtdTask(object):
-    def __init__(self, task_id, name, description='', priority=5):
+    def __init__(self, task_id, name, description='', priority=5, notify_callback=None):
         self.id = task_id
         self.name = name
         self.description = description
-        self.priority = priority
+        self._priority = priority
         self.done = False
+        self.callback = notify_callback
 
     def mark_as_done(self):
         self.done = True
 
-    def __cmp__(self, other):
-        if self.priority < other.priority:
-            return -1
-        elif self.priority == other.priority:
-            return 0
-        else:
-            return 1
+    @property
+    def priority(self):
+        return self._priority
+
+    @priority.setter
+    def priority(self, value):
+        old_value = self._priority
+        self._priority = value
+        self.notify(value, old_value)
+
+    def notify(self, new_value, old_value):
+        if self.callback is not None:
+            self.callback(self.id, new_value, old_value)
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def __gt__(self, other):
+        return self.priority > other.priority
+
+    def __eq__(self, other):
+        return self.priority == other.priority
 
 
 class PtdTodoList(object):
     def __init__(self, name):
         self.name = name
         self.tasks = []
+        self.ids = {}
+
+    def add_task(self, task):
+        if self.ids.get(task.id) is not None:
+            raise DuplicatedIdException()
+
+        def priority_change_callback(*args):
+            heapify(self.tasks)
+
+        task.callback = priority_change_callback
+        heappush(self.tasks, task)
+        self.ids[task.id] = task
+
--- test/test_ptd_todo_list.py
+++ test/test_ptd_todo_list.py
@@ -1,4 +1,7 @@
-from ptd.model.todo import PtdTodoList
+import pytest
+
+from ptd.model.todo import PtdTodoList, PtdTask
+from ptd.util.exceptions import DuplicatedIdException
 
 
 def test_todo_list_creation_default():
@@ -6,3 +9,85 @@ def test_todo_list_creation_default():
 
     assert todo_list.name == 'List Name'
     assert len(todo_list.tasks) == 0
+
+
+def test_todo_list_adding_task():
+    todo_list = PtdTodoList('List Name')
+    task = PtdTask(1, 'Test task')
+
+    todo_list.add_task(task)
+
+    assert len(todo_list.tasks) == 1
+    assert todo_list.tasks[0].id == 1
+    assert todo_list.tasks[0].name == 'Test task'
+
+
+def test_todo_list_adding_more_than_one_task():
+    todo_list = PtdTodoList('List Name')
+    task1 = PtdTask(1, 'Test task 1')
+    task2 = PtdTask(2, 'Test task 2')
+    task3 = PtdTask(3, 'Test task 3')
+
+    todo_list.add_task(task1)
+    todo_list.add_task(task2)
+    todo_list.add_task(task3)
+
+    assert len(todo_list.tasks) == 3
+    assert todo_list.tasks[0].id == 1
+    assert todo_list.tasks[0].name == 'Test task 1'
+    assert todo_list.tasks[1].id == 2
+    assert todo_list.tasks[1].name == 'Test task 2'
+    assert todo_list.tasks[2].id == 3
+    assert todo_list.tasks[2].name == 'Test task 3'
+
+
+def test_todo_list_adding_more_than_one_task_with_same_id_raises_exception():
+    todo_list = PtdTodoList('List Name')
+    task1 = PtdTask(1, 'Test task 1')
+    task2 = PtdTask(1, 'Test task 2')
+
+    todo_list.add_task(task1)
+
+    with pytest.raises(DuplicatedIdException):
+        todo_list.add_task(task2)
+
+
+def test_todo_list_adding_a_task_with_higher_priority():
+    todo_list = PtdTodoList('List Name')
+    task1 = PtdTask(1, 'Test task 1', priority=5)
+    task2 = PtdTask(2, 'Test task 2', priority=1)
+
+    todo_list.add_task(task1)
+    assert len(todo_list.tasks) == 1
+    assert todo_list.tasks[0].id == 1
+    assert todo_list.tasks[0].name == 'Test task 1'
+
+    todo_list.add_task(task2)
+    assert len(todo_list.tasks) == 2
+    assert todo_list.tasks[0].id == 2
+    assert todo_list.tasks[0].name == 'Test task 2'
+    assert todo_list.tasks[1].id == 1
+    assert todo_list.tasks[1].name == 'Test task 1'
+
+
+def test_todo_list_adding_two_tasks_and_modifying_priority_of_one():
+    todo_list = PtdTodoList('List Name')
+    task1 = PtdTask(1, 'Test task 1')
+    task2 = PtdTask(2, 'Test task 2')
+
+    todo_list.add_task(task1)
+    todo_list.add_task(task2)
+
+    assert len(todo_list.tasks) == 2
+    assert todo_list.tasks[0].id == 1
+    assert todo_list.tasks[0].name == 'Test task 1'
+    assert todo_list.tasks[1].id == 2
+    assert todo_list.tasks[1].name == 'Test task 2'
+
+    task2.priority = 1
+
+    assert len(todo_list.tasks) == 2
+    assert todo_list.tasks[0].id == 2
+    assert todo_list.tasks[0].name == 'Test task 2'
+    assert todo_list.tasks[1].id == 1
+    assert todo_list.tasks[1].name == 'Test task 1'
